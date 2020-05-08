7d030c914ef60f2c91c27db82095c733a4e58be4
Félix Richer <1804012@cstj.qc.ca>
2020-05-08 10:39:07 -0400

    réorganisation du dossier

--- 03_Production/01_laravel/resources/views/minimalDesign/vente/layers/APropos.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="aPropos">
-    <div class="txt_aPropos">
-        <h2>"à propos de nous."</h2>
-        <p>Nulla hendrerit vel velit dictum tempus. Nulla ligula tortor, dictum at tellus sed, dignissim scelerisque erat. Praesent pharetra pulvinar mauris quis suscipit. Curabitur varius sit amet diam a volutpat. Vivamus eget leo vel diam feugiat accumsan. Duis pharetra, velit quis varius pretium, massa libero elementum eros, ut dictum lacus ex sit amet mi. Etiam lobortis pulvinar dolor in tempus. Morbi auctor consectetur imperdiet. Phasellus sapien tortor, sodales eget mattis a, aliquam sed libero. Maecenas quis diam tristique, sollicitudin lorem ac, interdum magna. Aliquam pharetra, ligula sed varius tincidunt, tellus ex accumsan diam, vel sagittis nibh tortor et mauris.</p>
-        <p>Etiam vestibulum quis purus in ultricies. Suspendisse sed scelerisque lectus, id lobortis mi. Sed quis nibh sed ante imperdiet tristique eu ut risus. Suspendisse diam arcu, lobortis non odio ut, condimentum finibus elit. Morbi dui nunc, ultricies nec purus eget, faucibus consequat metus. Proin quis libero molestie, vestibulum velit at, laoreet ligula. Aliquam nunc nisl, euismod a dapibus sed, ullamcorper.</p>
-    </div>
-    <img src="" alt="">
-</section>
